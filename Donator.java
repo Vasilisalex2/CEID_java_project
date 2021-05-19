328e50388d455526f27d74d7d25467d32f9c5eac
NickVoulg02 <83508203+NickVoulg02@users.noreply.github.com>
2021-05-19 18:10:52 +0300

    Delete Donator.java

--- Donator.java
+++
@@ -1,9 +0,0 @@
-public class Donator extends User
-{
-    private Offers offerList;
-    public void AddOffer()
-    {
-        RequestDonation donation = new RequestDonation();
-        offerList.commit();
-    }
-}
